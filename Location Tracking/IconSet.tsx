d2fa964ed0b6ab7fc5f10aab8bc9554911dcc321
Mishika Sardana <148852225+Mishikasardana@users.noreply.github.com>
2025-01-13 19:43:32 +0530

    IconSet.tsx

---
+++ Location Tracking/IconSet.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const EmergencyIcon: React.FC = () => (
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+    <path d="M12 2L1 21h22L12 2zm0 3.45l8.4 14.55H3.6L12 5.45zm-1.4 6v4h2.8v-4h-2.8zm0 6v2h2.8v-2h-2.8z" fill="currentColor"/>
+  </svg>
+);
+
+export const LocationIcon: React.FC = () => (
+  <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+    <path d="M15 2C10.05 2 6 6.05 6 11c0 7.5 9 17 9 17s9-9.5 9-17c0-4.95-4.05-9-9-9zm0 12c-1.65 0-3-1.35-3-3s1.35-3 3-3 3 1.35 3 3-1.35 3-3 3z" fill="currentColor"/>
+  </svg>
+);
+
+export const NavigationIcon: React.FC = () => (
+  <svg width="100%" viewBox="0 0 480 108" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+    <path d="M240 0C107.452 0 0 24.253 0 54.167s107.452 54.166 240 54.166c132.548 0 240-24.252 240-54.166S372.548 0 240 0z" fill="#FFF"/>
+  </svg>
+);

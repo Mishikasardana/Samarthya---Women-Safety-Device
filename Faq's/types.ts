aa190a95db31e7eb6435557b5b90cb3c728930c7
Mishika Sardana <148852225+Mishikasardana@users.noreply.github.com>
2025-01-13 21:22:41 +0530

    types.ts

---
+++ Faq's/types.ts
@@ -0,0 +1,19 @@
+export interface IconProps {
+  src: string;
+  size: string;
+  alt: string;
+}
+
+export interface SafetyTipProps {
+  text: string;
+}
+
+export interface SafetyTipsData {
+  tips: string[];
+}
+
+export interface NavigationItem {
+  src: string;
+  size: string;
+  alt: string;
+}
